2cd77f8ca49b83bf2811a410e29fc491e41b142d
yuyi <962919526@qq.com>
2019-08-19 17:53:31 +0800

    modify loss function

--- auto-sync.py
+++ auto-sync.py
@@ -47,6 +47,7 @@ K.set_session(sess)
 x = tf.placeholder(tf.float32, shape=(None, INPUT_NODE))
 y_ = tf.placeholder(tf.float32, shape=(None, OUTPUT_NODE))
 
+# x = keras.layers.Input(shape=(INPUT_NODE,))
 layer1 = keras.layers.Dense(128, activation='relu')(x)
 
 layer1 = keras.layers.Dropout(DROP_OUT)(layer1)
@@ -59,7 +60,7 @@ layer3 = keras.layers.Dense(128, activation='relu')(layer2)
 
 layer3 = keras.layers.Dropout(DROP_OUT)(layer3)
 
-y = keras.layers.Dense(OUTPUT_NODE)(layer3)
+y = keras.layers.Dense(OUTPUT_NODE, activation='softmax')(layer3)
 
 # model = keras.models.Sequential()
 # model.add(keras.layers.normalization.BatchNormalization(input_shape=(INPUT_NODE,)))
@@ -67,22 +68,31 @@ y = keras.layers.Dense(OUTPUT_NODE)(layer3)
 # model.add(keras.layers.Dropout(DROP_OUT))
 # model.add(keras.layers.Dense(256, activation='relu'))
 # model.add(keras.layers.Dropout(DROP_OUT))
-# model.add(keras.layers.Dense(128,activation='relu'))
+# model.add(keras.layers.Dense(128, activation='relu'))
 # model.add(keras.layers.Dropout(DROP_OUT))
 # model.add(keras.layers.Dense(OUTPUT_NODE, activation='softmax'))
 
-#
+
+# def error_mean(y_ture, y_pred):
+#     return tf.reduce_mean(tf.cast(tf.equal(tf.argmax(y_ture, 1), tf.argmax(y_pred, 1)),
+#                                   dtype=tf.int32))
+
 # def error_num(y_ture, y_pred):
 #     return keras.backend.sum(keras.backend.square(y_pred - y_ture))
 #
+# model = keras.models.Model(inputs=x, outputs=y)
+#
 # model.compile(loss='categorical_crossentropy',
-#               optimizer=keras.optimizers.Adam(LEARNING_RATE, beta_1=0.9, beta_2=0.999, epsilon=1e-08),
-#               metrics=['accuracy', error_num])
+#               optimizer=keras.optimizers.Adam(LEARNING_RATE_BASE, beta_1=0.9, beta_2=0.999, epsilon=1e-08),
+#               metrics=['accuracy', error_mean])
 
-loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=y_,
-                                                                 logits=y))
 
-error_num = tf.reduce_sum(tf.square(y - y_)) / 2 / BATCH_SIZE
+loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=y,
+                                                                     labels=tf.argmax(y_, 1)))
+
+# loss = - tf.reduce_mean(y_ * tf.log(tf.clip_by_value(y, 1e-8, 1e2)))
+
+error_num = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1)), dtype=tf.int32))
 
 # 优化器
 # 定义当前迭代轮数的变量
@@ -99,52 +109,69 @@ learning_rate = tf.train.exponential_decay(LEARNING_RATE_BASE,
                                            staircase=STAIRCASE)
 
 # 定义优化函数
-train_step = tf.train.AdamOptimizer(learning_rate).minimize(loss, global_step)
+train_step = tf.train.AdamOptimizer(LEARNING_RATE_BASE).minimize(loss, global_step)
 
-# model.fit(x_train.values,
-#           y_train.values,
-#           batch_size=BATCH_SIZE,
-#           epochs = EPOCHS,
-#           verbose = 1,
-#           validation_data = (x_test.values, y_test.values),
-#           )
+# history_list = []
+# time_use = []
+# for snr in SNR:
+#     print('SNR:%d'%snr)
+#     start_time = time.time()
+#     X_train, X_test, Y_train, Y_test = read_data(snr)
+#     history = model.fit(
+#         X_train,
+#         Y_train,
+#         batch_size=BATCH_SIZE,
+#         epochs=EPOCHS,
+#         verbose=1,
+#         validation_data=(X_test, Y_test),
+#         )
+#     history_list.append(history)
+#     end_time = time.time()
+#     time_use.append(end_time-start_time)
+#     del(X_train, X_test, Y_train, Y_test)
+#
+# print(time_use)
+#
 
-
-snr_ber = []
 train_loss_list = []
 test_loss_list = []
 time_list = []
 
-# 保存开始时间
-time_start = time.time()
 
 for snr in SNR:
     train_loss_snr = []
     test_loss_snr = []
-    
-    X_train, X_test, Y_train, Y_test = read_data(snr)
 
+    X_train, X_test, Y_train, Y_test = read_data(snr)
+    # 保存开始时间
+    time_start = time.time()
     sess.run(tf.global_variables_initializer())  # 初始化
     for epoch in range(EPOCHS):
         for i in range(data_num // BATCH_SIZE):
             # 设置批次
+            test_start = int(data_num * (1 - test_rate))
             start = int((i * BATCH_SIZE) % data_num)
             end = min(start + BATCH_SIZE, data_num)
             
-            _ = sess.run(train_step,
+            if start < test_start:
+                sess.run(train_step,
                          feed_dict={x: X_train[start:end], y_: Y_train[start:end]})
-            
-            train_loss = sess.run(loss,
-                                  feed_dict={x: X_train[start:end], y_: Y_train[start:end]})
-            
-            test_start = int(data_num * (1-test_rate))
-            
-            if start >= test_start:
+    
+                train_loss = sess.run(loss,
+                                      feed_dict={x: X_train[start:end], y_: Y_train[start:end]})
+
+            else:
                 test_loss = sess.run(loss,
                                      feed_dict={x: X_test[start - test_start:end - test_start],
                                                 y_: Y_test[start - test_start:end - test_start]})
-            
+
             if i % 100 == 0:
+
+                # print('y:')
+                # print(sess.run(tf.argmax(y, 1), feed_dict={x: X_train[start:end], y_: Y_train[start:end]}))
+                # print('y_:')
+                # print(sess.run(tf.argmax(y_, 1), feed_dict={x: X_train[start:end], y_: Y_train[start:end]}))
+
                 if start >= test_start:
                     print('snr：%d, '
                           'epoch %d, '
@@ -177,36 +204,22 @@ for snr in SNR:
     time_list.append(time_end - time_start)
     train_loss_list.append(train_loss_snr)
     test_loss_list.append(test_loss_snr)
-    
 
-print('snr:', SNR)
-print('loss:', snr_ber)
-
-plt.figure(figsize=(10, 10))
-plt.plot(SNR, snr_ber)
-plt.grid()
-plt.xlabel('SNR')
-plt.ylabel('error_rate')
-plt.title('Deep Learning on channel estimate')
-plt.savefig('./error_rate' + '%s'%(time.strftime('%d_%H_%M')) + '.jpg')
-plt.show()
 
 pd.DataFrame(data=train_loss_list, index=SNR).T.plot()
 plt.title('Train loss')
 plt.grid()
-plt.xlabel('SNR')
-plt.xticks(SNR)
+plt.xlabel('steps')
 plt.ylabel('loss')
-plt.savefig('./train_loss' + '%s'%(time.strftime('%d_%H_%M')) + '.jpg')
+plt.savefig(r'./train_loss/train_loss' + '%s'%(time.strftime('%d_%H_%M')) + '.jpg')
 plt.show()
 
 pd.DataFrame(data=test_loss_list, index=SNR).T.plot()
 plt.title('Test loss')
 plt.grid()
-plt.xlabel('SNR')
-plt.xticks(SNR)
+plt.xlabel('steps')
 plt.ylabel('loss')
-plt.savefig('./test_loss' + '%s'%(time.strftime('%d_%H_%M')) + '.jpg')
+plt.savefig('./test_loss/test_loss' + '%s'%(time.strftime('%d_%H_%M')) + '.jpg')
 plt.show()
 
 pd.Series(data=time_list, index=SNR).plot()
@@ -214,5 +227,5 @@ plt.title('time using')
 plt.grid()
 plt.xlabel('SNR')
 plt.ylabel('time/s')
-plt.savefig('./time_use' + '%s'%(time.strftime('%d_%H_%M')) + '.jpg')
+plt.savefig('./time_use/time_use' + '%s'%(time.strftime('%d_%H_%M')) + '.jpg')
 plt.show()
